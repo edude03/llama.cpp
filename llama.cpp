d6fd67844c5ce0f8e1623e09d8b37deaa307c480
Francis Couture-Harpin <git@compilade.net>
2024-06-19 14:34:32 -0400

    wip : faster 1.625 bpw AVX2 vec_dot
    Not using a lookup table anymore makes it match q4_0 speed.
    
    * gguf-py : fix formatting
    
    * llama : remove spaces on empty line

--- llama.cpp
+++ llama.cpp
@@ -11699,7 +11699,7 @@ struct llm_build_context {
 
                 struct ggml_tensor *tmp = ggml_mul_mat(ctx0, model.layers[il].ffn_up, cur);
                 tmp = ggml_mul(ctx0, tmp, model.layers[il].ffn_up_scale);
-    
+
                 cb(tmp, "ffn_up", il);
 
                 cur = ggml_mul_mat(ctx0, model.layers[il].ffn_gate, cur);
